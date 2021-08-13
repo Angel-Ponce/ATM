39903e5de7ea12ae0248fd8ba19d01f3d16dd72a
Samantha0510 <64562679+Samantha0510@users.noreply.github.com>
2021-08-12 22:43:06 -0600

    Add Views

--- src/View/View.java
+++ src/View/View.java
@@ -6,4 +6,18 @@ package View;
  */
 public class View {
 
+    public static ATMView atmView = new ATMView();
+    public static AddCashView addCashView = new AddCashView();
+    public static BalanceView balanceView = new BalanceView();
+    public static ChangeCardNumberView changeCardNumberView = new ChangeCardNumberView();
+    public static ChangeMaximumAmountView changeMaximumAmountView = new ChangeMaximumAmountView();
+    public static ChangePinView changePinView = new ChangePinView();
+    public static CreateUserView createUserView = new CreateUserView();
+    public static DepositView depositView = new DepositView();
+    public static InitATMView initATMView = new InitATMView();
+    public static LastTransactionsView lastTransactionsView = new LastTransactionsView();
+    public static LoginView loginView = new LoginView();
+    public static RetreatView retreatView = new RetreatView();
+    public static UserConsultView userConsultView = new UserConsultView();
+    public static UserControlView userControlView = new UserControlView();
 }
