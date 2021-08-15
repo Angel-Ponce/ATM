fc2ee51c194cb166ba294d2b90609c1a358657c5
Samantha0510 <64562679+Samantha0510@users.noreply.github.com>
2021-08-14 21:49:53 -0600

    add user

--- src/Others/Main.java
+++ src/Others/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         LoginController loginController = new LoginController();
         loginController.start();
+        
     }
 
     private static void register() {
