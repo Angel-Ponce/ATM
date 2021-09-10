e9e91b454003585cd803618325927b78c3655727
Angel Ponce <acdpf000@gmail.com>
2021-09-09 19:14:45 -0600

    Adding Connecter class to stablish a connection with SQL Server

---
+++ src/Others/Connecter.java
@@ -0,0 +1,33 @@
+package Others;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Angel Ponce
+ */
+public class Connecter {
+
+    private static final String URL = "jdbc:sqlserver://localhost:1434;databaseName=ATM;";
+    private static final String USER = "angelATM";
+    private static final String PASSWORD = "angel123";
+    public PreparedStatement ps;
+    public ResultSet rs;
+    public Connection con;
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace(System.err);
+        }
+        return connection;
+    }
+
+}
