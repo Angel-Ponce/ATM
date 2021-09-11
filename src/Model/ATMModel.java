e3e137e73f583545a0e07071aa2121709c7317bc
Samantha0510 <64562679+Samantha0510@users.noreply.github.com>
2021-09-10 23:40:44 -0600

    add test off tickets to database

--- src/Model/ATMModel.java
+++ src/Model/ATMModel.java
@@ -5,6 +5,7 @@ import Controller.LoginController;
 import Entity.Person;
 import Entity.Properties;
 import Entity.Ticket;
+import Others.Connecter;
 import Others.Helper;
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,8 +19,30 @@ public class ATMModel {
     public static void logout(Person lastPerson) {
         ATMController.properties.setLastPerson(lastPerson);
         Helper.saveObjectToFile(ATMController.tickets, "database/Tickets.txt");
+          try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            c.ps = c.con.prepareStatement("UPDATE ticket SET size='"+ATMController.tickets+"'");
+          } catch(Exception e){
+              
+          }
         Helper.saveObjectToFile(ATMController.properties, "database/Properties.txt");
+         try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            c.ps = c.con.prepareStatement("UPDATE ticket SET current_balance='"+ATMController.properties+"'");
+          } catch(Exception e){
+              
+          }
+          
         Helper.saveObjectToFile(LoginController.persons, "database/Persons.txt");
+      try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            c.ps = c.con.prepareStatement("UPDATE ticket SET card_number,current_balance,maximum_amount,pin,last_access='"+LoginController.persons+"'");
+          } catch(Exception e){
+              
+          }
     }
 
     public static ArrayList<Ticket> getTickets() {
