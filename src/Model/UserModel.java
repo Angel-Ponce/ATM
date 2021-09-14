d03a6018fea01d61060b43279aa0bf4274e5e1f8
Angel Ponce <acdpf000@gmail.com>
2021-09-13 21:46:16 -0600

    Retreats of database, adding transaction method to add new transactions to database

--- src/Model/UserModel.java
+++ src/Model/UserModel.java
@@ -8,7 +8,9 @@ import Entity.User;
 import Others.Connecter;
 import Others.Helper;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -46,13 +48,31 @@ public class UserModel {
         } catch (SQLException e) {
             System.err.println(e);
         }
-        return Helper.saveObjectToFile(LoginController.persons, "database/Persons.txt");
-
+        return false;
     }
 
     public static boolean retreat(int amount, Person person) {
-        Helper.personToUser(person).retreat(amount);
-        return Helper.saveObjectToFile(LoginController.persons, "database/Persons.txt");
+
+        try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            if (person instanceof User) {
+                ((User) person).retreat(amount);
+                c.ps = c.con.prepareStatement("UPDATE \"user\" SET current_balance = ? WHERE card_number = ?");
+                c.ps.setInt(1, ((User) person).getCurrentBalance());
+                c.ps.setLong(2, ((User) person).getCardNumber());
+                insertTransaction(Transaction.RETREAT, amount, new Date(), ((User) person).getCardNumber());
+                int r = c.ps.executeUpdate();
+                if (r > 0) {
+                    c.con.close();
+                    return true;
+                }
+                c.con.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return false;
     }
 
     public static boolean deposit(int amount, Person person) {
@@ -62,5 +82,21 @@ public class UserModel {
 
     public static ArrayList<Transaction> getLatestTransactions(Person person) {
         return null;
+    }
+
+    public static void insertTransaction(String type, int amount, Date date, long cardNumber) {
+        Timestamp time = Timestamp.valueOf(Model.TIMESTAMP.format(date));
+        try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            c.ps = c.con.prepareStatement("INSERT INTO transaction(type,amount,date,card_number) VALUES(?,?,?,?)");
+            c.ps.setString(1, type);
+            c.ps.setInt(2, amount);
+            c.ps.setTimestamp(3, time);
+            c.ps.setLong(4, cardNumber);
+            c.con.close();
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
     }
 }
