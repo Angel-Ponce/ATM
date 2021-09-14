491606b326ff15f1fc36309698dea824dda6e74e
Angel Ponce <acdpf000@gmail.com>
2021-09-13 21:34:18 -0600

    Updating pin into database

--- src/Model/UserModel.java
+++ src/Model/UserModel.java
@@ -1,9 +1,13 @@
 package Model;
 
 import Controller.LoginController;
+import Entity.Admin;
 import Entity.Person;
 import Entity.Transaction;
+import Entity.User;
+import Others.Connecter;
 import Others.Helper;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -13,7 +17,35 @@ import java.util.ArrayList;
 public class UserModel {
 
     public static boolean changePin(int newPin, Person person) {
-        person.setPin(newPin);
+        try {
+            Connecter c = new Connecter();
+            c.con = c.getConnection();
+            if (person instanceof User) {
+                c.ps = c.con.prepareStatement("UPDATE \"user\" SET pin = ? WHERE card_number = ?");
+                c.ps.setInt(1, newPin);
+                c.ps.setLong(2, ((User) person).getCardNumber());
+                int r = c.ps.executeUpdate();
+                if (r > 0) {
+                    person.setPin(newPin);
+                    c.con.close();
+                    return true;
+                }
+                c.con.close();
+            } else if (person instanceof Admin) {
+                c.ps = c.con.prepareStatement("UPDATE \"admin\" SET pin = ? WHERE email = ?");
+                c.ps.setInt(1, newPin);
+                c.ps.setString(2, person.getEmail());
+                int r = c.ps.executeUpdate();
+                if (r > 0) {
+                    person.setPin(newPin);
+                    c.con.close();
+                    return true;
+                }
+                c.con.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
         return Helper.saveObjectToFile(LoginController.persons, "database/Persons.txt");
 
     }
